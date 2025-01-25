39cd5914293e375cd17eb058971d621b8da74d51
kbramh <kylebramhall2@hotmail.com>
2025-01-25 14:52:26 -0800

    initialized stripe and checkout

---
+++ public/checkout.js
@@ -0,0 +1,24 @@
+const stripe = Stripe("pk_test_51QNHGyJdgZWAkXVlt23ESao7Ho2LrYPRcbxRFXMTcQJxQRceC2nKOJMfvNqLPRKuUBNBJSGF7uPVaiebsRxvWPUX00tOJTL26l")
+
+
+
+
+initialize(); 
+
+//Create a Checkout Session
+async function initialize() {
+    const fetchClientSecret = async () => {
+        const reponse = await fetch("/create-checkout-session", {
+            method: 'POST', 
+        });  
+        const { clientSecret } = await response.json(); 
+        return clientSecret; 
+    }; 
+
+const checkout = await stripe.initEmbeddedCheckout({
+    fetchClientSecret,
+}); 
+
+//Mount Checkout
+checkout.mount('#checkout'); 
+}
