bf99621d45e4169406e34174f0efc9d06a434f95
kbramh <kylebramhall2@hotmail.com>
2025-01-25 15:19:20 -0800

    added code to return.html file

--- public/checkout.js
+++ public/checkout.js
@@ -1,20 +1,22 @@
-const stripe = Stripe("pk_test_51QNHGyJdgZWAkXVlt23ESao7Ho2LrYPRcbxRFXMTcQJxQRceC2nKOJMfvNqLPRKuUBNBJSGF7uPVaiebsRxvWPUX00tOJTL26l")
+const stripe = Stripe(
+  'pk_test_51QNHGyJdgZWAkXVlt23ESao7Ho2LrYPRcbxRFXMTcQJxQRceC2nKOJMfvNqLPRKuUBNBJSGF7uPVaiebsRxvWPUX00tOJTL26l'
+);
 initialize();
 
 //Create a Checkout Session
 async function initialize() {
-    const fetchClientSecret = async () => {
-        const response = await fetch("/create-checkout-session", {
-            method: 'POST',
-        });
-        const { clientSecret } = await response.json();
-        return clientSecret;
-    };
-
-    const checkout = await stripe.initEmbeddedCheckout({
-        fetchClientSecret,
+  const fetchClientSecret = async () => {
+    const response = await fetch('/create-checkout-session', {
+      method: 'POST',
     });
+    const { clientSecret } = await response.json();
+    return clientSecret;
+  };
 
-    //Mount Checkout
-    checkout.mount('#checkout');
+  const checkout = await stripe.initEmbeddedCheckout({
+    fetchClientSecret,
+  });
+
+  //Mount Checkout
+  checkout.mount('#checkout');
 }
